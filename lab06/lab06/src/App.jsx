305c4838cd89560c581cae8a8f6db9c55bfd4b3f
Truongxt <truongxt2501@gmail.com>
2025-04-09 17:07:32 +0700

    6. React router

--- lab06/lab06/src/App.jsx
+++ lab06/lab06/src/App.jsx
@@ -1,10 +1,26 @@
 import LayoutDefault from "./components/LayoutDefault"
 import "./App.css"
+import { Route, Routes } from "react-router"
+import Dashboard from "./pages/Dashboard"
+import Analyst from "./pages/Analyst"
+import Project from "./pages/Project"
+import Message from "./pages/Message"
+import Team from "./pages/Team"
+import Intergration from "./pages/Intergration"
 function App() {
 
   return (
     <>
-      <LayoutDefault/>
+      <Routes>
+        <Route path='/' element={<LayoutDefault />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/analyst" element={<Analyst />} />
+          <Route path="/integration" element={<Intergration />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/message" element={<Message />} />
+          <Route path="/team" element={<Team />} />
+        </Route>
+      </Routes>
     </>
   )
 }
