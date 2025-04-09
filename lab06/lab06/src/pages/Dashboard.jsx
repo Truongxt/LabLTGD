b65357815f46d9be00ef2b5c6f2f61223463eb89
Truongxt <truongxt2501@gmail.com>
2025-04-09 16:57:36 +0700

    3. Data table react

--- lab06/lab06/src/pages/Dashboard.jsx
+++ lab06/lab06/src/pages/Dashboard.jsx
@@ -2,20 +2,6 @@ import { useEffect, useState } from "react";
 
 function Dashboard() {
     const [data, setData] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [showModal, setShowModal] = useState(false);
-    const [editingRow, setEditingRow] = useState(null);
-    const [isEditing, setIsEditing] = useState(false);
-
-    const rowsPerPage = 5;
-    const indexOfLastRow = currentPage * rowsPerPage;
-    const indexOfFirstRow = indexOfLastRow - rowsPerPage;
-    const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
-    const totalPages = Math.ceil(data.length / rowsPerPage);
-
-    const handlePageClick = (pageNumber) => {
-        setCurrentPage(pageNumber);
-    };
 
     useEffect(() => {
         fetch("http://localhost:3000/orders")
@@ -27,9 +13,27 @@ function Dashboard() {
 
     return (
         <div>
-           
-        </div>
+            <div>
+                <div style={{ marginBottom: 10, textAlign: "right" }}>
+                </div>
+                <table id="myTable" className="display">
+                    <thead>
+                        <tr >
+                            <th>CUSTOMER NAME</th>
+                            <th>COMPANY</th>
+                            <th>ORDER VALUE</th>
+                            <th>ORDER DATE</th>
+                            <th>STATUS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       
+                    </tbody>
+                </table>
+
+            </div>
+            </div>
     )
 }
 
-export default Dashboard
+            export default Dashboard
